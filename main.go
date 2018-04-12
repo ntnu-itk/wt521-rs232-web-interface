33b7573ec6447761f77e5d7d84a8fce0aa5abc69
Åsmund Stavdahl <smund3@gmail.com>
2018-04-12 13:27:06 +0200

    complete basic functionality with JSON (/json) and HTML (/) outputs

--- main.go
+++ main.go
@@ -1,21 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/tarm/serial"
 )
 
 type State struct {
-	data string
+	windAngle   int16
+	windSpeed   float64
+	lastUpdated time.Time
 }
 
-type StatePatch struct {
-	newState State
-}
+type StatePatch State
 
 type PatchChannel chan StatePatch
 type StateChannel chan State
@@ -25,94 +26,173 @@ var patchChannel PatchChannel
 var stateChannel StateChannel
 var requestChannel RequestChannel
 
+var flagName string
+var flagBaud int
+var flagPort int
+
+func openSerialPort() (*serial.Port, error) {
+	return serial.OpenPort(
+		&serial.Config{
+			Name:     flagName,
+			Baud:     flagBaud,
+			Parity:   serial.ParityNone,
+			StopBits: serial.Stop1,
+			Size:     8})
+}
+
+func handleFlags() {
+	flag.StringVar(&flagName, "dev", "/dev/ttyS0", "serial port to use")
+	flag.IntVar(&flagBaud, "baud", 9600, "baud rate (WT521's facotry default is 1200 but it should be reconfigured to 9600)")
+	flag.IntVar(&flagPort, "port", 8080, "port to open for HTTP server")
+}
+
 func main() {
 	patchChannel = make(PatchChannel, 1)
 	stateChannel = make(StateChannel, 0)
 	requestChannel = make(RequestChannel, 0)
 
-	go stateKeeper(patchChannel, requestChannel, stateChannel)
-	go serialMonitor(patchChannel)
+	handleFlags()
 
-	log.Fatal(httpServer(requestChannel, stateChannel))
-}
-
-func serialMonitor(patchChannel PatchChannel) {
-	var c *serial.Config
-	var s *serial.Port
-	var err error
-	buf := make([]byte, 50)
-
-	c = &serial.Config{
-		Name:   "/dev/ttyS0",
-		Baud:   300,
-		Parity: serial.ParityEven,
-		//StopBits: serial.Stop1,
-		Size: 7}
-	s, err = serial.OpenPort(c)
+	serialPort, err := openSerialPort()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	go stateKeeper(patchChannel, requestChannel, stateChannel)
+	go serialMonitor(serialPort, patchChannel)
+
+	log.Fatal(httpServer(requestChannel, stateChannel))
+}
+
+func serialMonitor(serialPort *serial.Port, patchChannel PatchChannel) {
+	patch := &StatePatch{}
+
 	for true {
-		charBuf := make([]byte, 1)
-		for true {
-			//n, _ :=
-			s.Read(charBuf)
-			if string(charBuf) == "$" {
-				break
-			}
-		}
-
-		bytesRead := 0
-
-		for bytesRead < 31 {
-			n, err := s.Read(buf[bytesRead:])
-			bytesRead += n
-			if err != nil {
-				log.Fatal(err)
-			}
-		}
-		fmt.Printf("%d*c: %s\n", bytesRead, buf)
+		patch.parse(serialPort)
 		select {
 		case <-patchChannel:
-			log.Println("Discarded a patch")
+			log.Println("Discarded a patch. The stateKeeper goroutine may have crashed.")
 		default:
-			log.Println("No patch in queue")
+			//log.Println("No patch in queue")
 		}
-		log.Println("Sending patch")
-		patchChannel <- StatePatch{
-			newState: State{
-				data: string(buf)}}
-		log.Println("Sent patch")
+		patchChannel <- *patch
 	}
 }
 
 func stateKeeper(patchChannel PatchChannel, requestChannel RequestChannel, stateChannel StateChannel) {
-	var state State
-	var patch StatePatch
+	state := State{}
+	patch := StatePatch{}
 
 	for {
 		select {
 		case patch = <-patchChannel:
-			log.Println("Got patch")
-			state = patch.newState
-			log.Printf("New state: %s", state.data)
+			state.patch(patch)
 		case <-requestChannel:
-			log.Println("Got request")
 			stateChannel <- state
-			log.Println("Sent state")
 		}
+		log.Println(&state)
 	}
 }
 
 func httpServer(requestChannel RequestChannel, stateChannel StateChannel) error {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("Got request for %s\n", r.URL.Path)
+	http.HandleFunc("/json", func(w http.ResponseWriter, r *http.Request) {
 		requestChannel <- true
-		log.Println("Sent request")
 		state := <-stateChannel
-		log.Println("Got state")
-		fmt.Fprintf(w, "<!DOCTYPE html><html><body>Hello, %q\n<br>\nState: %s", html.EscapeString(r.URL.Path), state.data)
+		log.Printf("Got state %s\n", state.String())
+
+		jsonString := fmt.Sprintf(`{
+    "speed": %.1f,
+    "angle": %d,
+    "updated": "%s"
+}`,
+			state.windSpeed,
+			state.windAngle,
+			state.lastUpdated)
+
+		log.Println(jsonString)
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprint(w, jsonString)
 	})
-	return http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		requestChannel <- true
+		state := <-stateChannel
+		log.Printf("Got state %s\n", state.String())
+
+		jsonString := fmt.Sprintf(`<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+</head>
+<body>
+    <h1>Værinfo</h1>
+    Speed: %.1f
+    <br>
+    Angle: %d
+    <br>
+    Updated: "%s
+    <script>setTimeout('location.reload()', 1010)</script>`,
+			state.windSpeed,
+			state.windAngle,
+			state.lastUpdated)
+
+		log.Println(jsonString)
+		w.Header().Set("Content-Type", "text/html")
+		fmt.Fprint(w, jsonString)
+	})
+	return http.ListenAndServe(fmt.Sprintf(":%d", flagPort), nil)
+}
+
+func (patch *StatePatch) parse(serialPort *serial.Port) error {
+	b := make([]byte, 500)
+	for b[0] != byte('$') {
+		serialPort.Read(b[0:1])
+	}
+
+	bytesRead := 0
+	for {
+		n, err := serialPort.Read(b[bytesRead:])
+		bytesRead += n
+		if err != nil {
+			log.Printf("Error reading 'till newline: %s", err)
+		}
+		if rune(b[bytesRead-1]) == '*' {
+			log.Printf("Have read %d bytes: %s", bytesRead, b)
+			break
+		}
+		if bytesRead == cap(b) {
+			log.Printf("Warning: read the maximum of %d bytes into buffer but no delimiter found; bailing early. If this happens consistently, check if serial port is configured correctly.", bytesRead)
+			log.Printf("Warning (cont.): our buffer contains this: %s", string(b))
+			break
+		}
+	}
+
+	n, err := fmt.Sscanf(string(b),
+		"WIMWV,%d,R,%f,M,A*",
+		&patch.windAngle,
+		&patch.windSpeed)
+
+	if n != 2 || err != nil {
+		log.Printf("Failed to extract data from buffer. Was able to parse %d of 2 numbers of interest with error '%s'", n, err)
+	}
+
+	return err
+}
+
+func (state *State) patch(patch StatePatch) {
+	state.windSpeed = patch.windSpeed
+	state.windAngle = patch.windAngle
+	state.lastUpdated = time.Now()
+}
+
+func (state *State) String() string {
+	return fmt.Sprintf("State{ Speed:%f  Angle:%d  Updated:%s }",
+		state.windSpeed,
+		state.windAngle,
+		state.lastUpdated)
+}
+
+func (patch *StatePatch) String() string {
+	return fmt.Sprintf("StatePatch{ Speed:%f  Angle:%d }",
+		patch.windSpeed,
+		patch.windAngle)
 }
